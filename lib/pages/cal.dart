4eaf66b39ad5a565566b86eccadd162c4a1d1dbe
tananonid <tananon2002@gmail.com>
2024-01-27 12:15:02 +0700

    add main.dart

---
+++ lib/pages/cal.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class CalculatorButton extends StatelessWidget {
+  final String text;
+  final int fillColor;
+  final int textColor;
+  final double textSize;
+  final Function callback;
+
+  const CalcButton({
+    Key key,
+    this.text,
+    this.fillColor,
+    this.textColor = 0xFF000000,
+    this.textSize = 28,
+    this.callback,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(10),
+      child: SizedBox(
+        width: 65,
+        height: 65,
+        child: FloatingActionButton(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(0),
+          ),
+          onPressed: () {
+            callback(text);
+          },
+          child: Text(
+            text,
+            style: GoogleFonts.rubik(
+              textStyle: TextStyle(
+                fontSize: textSize,
+              ),
+            ),
+          ),
+          color: fillColor != null ? Color(fillColor) : null,
+          textColor: Color(textColor),
+        ),
+      ),
+    );
+  }
+}
